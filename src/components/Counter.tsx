a0f394a1026feb164d7444d87ec252450988ea4c
Kristiyan Kiryakov <emineo@abv.bg>
2023-12-16 13:21:16 +0200

    add buttons and attach actions

---
+++ src/components/Counter.tsx
@@ -0,0 +1,21 @@
+import {useDispatch, useSelector} from 'react-redux'
+import {RootState} from '../state/store';
+import {decrement, increment} from '../state/counter/counterSlice';
+
+const Counter = () => {
+
+    const count = useSelector((state: RootState) => state.counter.value);
+    const dispatch = useDispatch();
+
+    return (
+        <div>
+            {count}
+            <div>
+                <button onClick={() => dispatch(increment())} >Increment</button>
+                <button onClick={() => dispatch(decrement())} >Decrement</button>
+            </div>
+        </div>
+    )
+}
+
+export default Counter
--- src/state/counter/counterSlice.ts
+++ src/state/counter/counterSlice.ts
@@ -12,11 +12,16 @@ export const counterSlice = createSlice({
     name: 'counter',
     initialState,
     reducers: {
+        // createSlice makes copy of the state under the hood and this code is immutable
         increment: (state) => {
-            // createSlice makes copy of the state under the hood and this code is immutable
             state.value += 1;
+        },
+        decrement: (state) => {
+            state.value -= 1;
         }
     }
 });
+
+export const {increment, decrement} = counterSlice.actions;
 
 export default counterSlice.reducer;
